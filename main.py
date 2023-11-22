30911ca7de17d48661d80316ca23c1e308334e24
G_oaT <solarcrash3@gmail.com>
2023-11-23 01:19:55 +0300

    loan calculator

--- main.py
+++ main.py
@@ -1,73 +1,63 @@
 
-# Building a  command Line Application in python (bbok store application)
-
-def  main():
-    
+def main():
     try:
-    
-        booklist =[]  #initalizing thebooklist
-        
-        infile= open("Booklists.txt", "r")
-        line= infile.readline()
-        
+        booklist = []  # Initializing the booklist
+
+        infile = open("Booklists.txt", "r")
+        line = infile.readline()
+
         while line:
             booklist.append(line.rstrip("\n").split(","))
-            line =infile.readline()
-        infile.close() # Closing  the infile
+            line = infile.readline()
+        infile.close()  # Closing the infile
+
     except FileNotFoundError:
-        print("An unexpected error occurred and your <Booklists.txt> file  is not found.")
-    
+        print("An unexpected error occurred and your <Booklists.txt> file is not found.")
         print("Starting a new bookslist")
-        booklist=[]
-        
-    choice =0
-    while choice!=4:
+        booklist = []
+
+    choice = 0
+    while  choice!=4:
         print("Greetings Developer")
-        print(" ***Welcome to the Book  Manager :)")
-        print("1)Add a book")
-        print("2)Look up a book")
-        print("3)Display  books")
+        print(" ***Welcome to the Book Manager :)")
+        print("1) Add a book")
+        print("2) Look up a book")
+        print("3) Display books")
         print("4) Quit")
-        choice =int(input())
-        
+        choice = int(input())
+
         if choice == 1:
             print("Adding a book...")
             nbook = input("Enter the name of the book >>")
-            nAuthor= input("Enter the Author of the Book>>")
+            nAuthor = input("Enter the Author of the Book >>")
             nPages = input("Enter the Number of pages in the book >>")
-            
-            booklist.append([nbook,nAuthor,nPages])
-        
-        elif choice ==2:
+
+            booklist.append([nbook, nAuthor, nPages])
+
+        elif choice == 2:
             print("Looking up for a book ...")
-            keyword=input("Enter search Term to look for your book :")
-            
+            keyword = input("Enter search Term to look for your book:")
             for book in booklist:
                 if keyword in book:
                     print(book)
-        elif choice==3:
-            print("Displaying all  your books ...")
+
+        elif choice == 3:
+            print("Displaying all your books ...")
             for i in range(len(booklist)):
-            
-              print(booklist[i])
-              
+                print(booklist[i])
+
         else:
-            
             print("Leaving Bookstore...")
             quit()
-            
+
     print("Program terminated..")
-# Saving the books listed to an external TXT  File
-    outfile =open("Booklists.txt","W")
-    for book in  booklist :
-        outfile.write("," .join(book) + "\n")
+
+    # Saving the books listed to an external TXT File
+    outfile = open("Booklists.txt", "w")
+    for book in booklist:
+        outfile.write(",".join(book) + "\n")
     outfile.close()
-        
-                
-            
-        
 
 
-if   __name__ == "__main__":
-    main() 
-    
+if __name__ == "__main__":
+    main()
